9186f78a8a1ea30eae39c2c5cae97897398bec04
Urban Sanden <hej@urre.me>
2019-06-04 17:10:22 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            url: 'https://css-tricks.com'
+        }
+    }
+
+    filterHistory = event => {
+        this.setState({ url: event.currentTarget.value.toLowerCase() })
+        this.setQueryString(event.currentTarget.value.toLowerCase())
+    }
+
+    getQueryString() {
+        const urlParams = new URLSearchParams(window.location.search)
+        return urlParams.get('url')
+    }
+
+    setQueryString(url) {
+        window.history.pushState('', '', `?url=${url}`)
+        this.setState({ url: url })
+    }
+
+    componentDidMount() {
+        this.getQueryString() && this.setQueryString(this.getQueryString());
+    }
+
+    render() {
+        return (
+            <main>
+                <header className="header">
+                    <input
+                        type="text"
+                        onChange={this.filterHistory}
+                        placeholder="Paste URL"
+                        disableautocomplete="true"
+                        autoComplete="off"
+                        data-lpignore="true"
+                        spellCheck="false"
+                        autoFocus
+                        value={this.state.url}
+                    />
+                </header>
+
+                <div className="environment">
+                <div className="phone"><div className="phone-screen">
+                                            <iframe
+                                title="Website"
+                                id="frame-content"
+                                sandbox="allow-scripts allow-same-origin"
+                                className="style-scope device-view"
+                                src={this.state.url}
+                            />
+                </div></div>
+                    <div className="window">
+                        <div className="device">
+                            <iframe
+                                title="Website"
+                                id="frame-content"
+                                sandbox="allow-scripts allow-same-origin"
+                                className="style-scope device-view"
+                                src={this.state.url}
+                            />
+                        </div>
+                    </div>
+
+                </div>
+
+                <footer>
+                    <p>Try your site in a small computer on a desk. * Many sites don't allow their sites to be loaded in iframes.</p>
+                    <p>10 min project by <a href="https://twitter.com/urre">@urre</a>. This site is not responsive ;). <a href="https://unsplash.com/@agkdesign?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge">Photo: agkdesign</a></p>
+                </footer>
+            </main>
+        )
+    }
 }
 
-export default App;
+export default App
